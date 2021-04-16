d3427bc228d69359f62783865ae54dda35df6abc
shell-escape <shell-escape@yandex.ru>
2021-04-16 17:02:50 +0300

    Changed docsting and other improvements

--- tests/homework_1/test_task_4.py
+++ tests/homework_1/test_task_4.py
@@ -2,13 +2,15 @@ from homework_1.task_4 import check_sum_of_four
 
 
 def test_one_int_positive_case():
-    """Testing a case with one integer in each list that satisfies the condition"""
+    """Testing a case with one integer in each list
+    that satisfies the condition"""
     a, b, c, d = [0], [0], [0], [0]
     assert check_sum_of_four(a, b, c, d) == 1
 
 
 def test_one_int_negative_case():
-    """Testing a case with one integer in each list that does not satisfy the condition"""
+    """Testing a case with one integer in each list
+    that does not satisfy the condition"""
     a, b, c, d = [1], [1], [1], [1]
     assert check_sum_of_four(a, b, c, d) == 0
 
